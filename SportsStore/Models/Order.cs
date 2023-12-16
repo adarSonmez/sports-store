d1d06ef48520380d4fed7e2a76cc28243ddaa95c
adarSonmez <adarsonmez@outlook.com>
2023-12-16 17:06:17 +0300

    Secure user input

--- SportsStore/Models/Order.cs
+++ SportsStore/Models/Order.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace SportsStore.Models;
@@ -6,31 +7,39 @@ namespace SportsStore.Models;
 // Model class representing an order in the application.
 public class Order
 {
-    [BindNever]
-    public int OrderId { get; set; }
+    [BindNever] // Prevents the user from supplying a value for this property.
+    public long OrderId { get; set; }
 
-    [BindNever]
+    [BindNever] 
     public ICollection<CartLine> Lines { get; set; } = new List<CartLine>();
 
     [Required(ErrorMessage = "Please enter a name")]
+    [Column(TypeName = "nvarchar(128)")]
     public string? Name { get; set; }
 
     [Required(ErrorMessage = "Please enter the first address line")]
+    [Column(TypeName = "nvarchar(512)")]
     public string? Line1 { get; set; }
 
+    [Column(TypeName = "nvarchar(512)")]
     public string? Line2 { get; set; }
 
+    [Column(TypeName = "nvarchar(512)")]
     public string? Line3 { get; set; }
 
     [Required(ErrorMessage = "Please enter a city name")]
+    [Column(TypeName = "nvarchar(128)")]
     public string? City { get; set; }
 
     [Required(ErrorMessage = "Please enter a state name")]
+    [Column(TypeName = "nvarchar(128)")]
     public string? State { get; set; }
 
+    [Column(TypeName = "nvarchar(128)")]
     public string? Zip { get; set; }
 
     [Required(ErrorMessage = "Please enter a country name")]
+    [Column(TypeName = "nvarchar(128)")]
     public string? Country { get; set; }
 
     public bool GiftWrap { get; set; }
--- SportsStore/Models/Product.cs
+++ SportsStore/Models/Product.cs
@@ -7,12 +7,15 @@ public class Product
 {
     public long? ProductId { get; set; }
 
+    [Column(TypeName = "nvarchar(128)")]
     public string Name { get; set; } = string.Empty;
 
+    [Column(TypeName = "nvarchar(1024)")]
     public string Description { get; set; } = string.Empty;
 
     [Column(TypeName = "decimal(8, 2)")] 
     public decimal Price { get; set; }
     
+    [Column(TypeName = "nvarchar(128)")]
     public string Category { get; set; } = string.Empty;
 }
